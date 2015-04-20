24793097284ffb6f461b34cffeff347df07386f6
Lorenzo Bolla <lbolla@gmail.com>
2015-04-20 09:17:35 +0100

    Removed python 3.3 from supported version because it's not tested

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: Physics',
